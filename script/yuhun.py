afbb9a539730df4e62a1c25c966239a281998532
leevccc <94272011+leevccc@users.noreply.github.com>
2022-11-26 02:04:16 +0800

    feat:御魂胚子识别

--- script/yuhun.py
+++ script/yuhun.py
@@ -2,7 +2,7 @@ import aircv
 import cv2
 import numpy as np
 import pyautogui
-
+import xlwings as xw
 import app
 import logger
 from script import window
@@ -78,7 +78,6 @@ mainList = {
     "mXiaoGuoMingZhong": "效果命中",
     "mXiaoGuoDiKang": "效果抵抗",
 }
-
 fuList = {
     "fGongJi": "攻击",
     "fFangYu": "防御",
@@ -91,6 +90,50 @@ fuList = {
     "fShengMingJiaCheng": "生命加成",
     "fXiaoGuoMingZhong": "效果命中",
     "fXiaoGuoDiKang": "效果抵抗",
+}
+
+keyList = {
+    "攻": "攻击加成",
+    "防": "防御加成",
+    "生": "生命加成",
+    "速": "速度",
+    "暴": "暴击",
+    "爆": "暴击伤害",
+    "命": "效果命中",
+    "抵": "效果抵抗",
+    "攻击加成": "攻击加成",
+    "防御加成": "防御加成",
+    "生命加成": "生命加成",
+    "速度": "速度",
+    "暴击": "暴击",
+    "暴击伤害": "暴击伤害",
+    "效果命中": "效果命中",
+    "效果抵抗": "效果抵抗",
+}
+
+indexList = {
+    "输出": "伤害输出",
+    "命": "效果命中",
+    "命中": "效果命中",
+    "抵": "效果抵抗",
+    "抵抗": "效果抵抗",
+    "生": "生命加成",
+    "攻": "攻击加成",
+    "防": "防御加成",
+    "速": "速度",
+    "暴": "暴击",
+    "爆": "暴击伤害",
+    "治疗": "治疗量",
+    "伤害输出": "伤害输出",
+    "效果命中": "效果命中",
+    "效果抵抗": "效果抵抗",
+    "生命": "生命加成",
+    "攻击": "攻击加成",
+    "防御": "防御加成",
+    "速度": "速度",
+    "暴击": "暴击",
+    "暴击伤害": "暴击伤害",
+    "治疗量": "治疗量",
 }
 
 
@@ -114,7 +157,7 @@ def initial():
         init = True
 
 
-def ana():
+def readYuHun():
     initial()  # 初始化加载各种图片素材
 
     # 获取"清理图标"的坐标信息
@@ -201,3 +244,103 @@ def getImgObjectByFileName(fileName):
 def findImgByImgObject(imgObject, screenshot, confidence=0.90):
     match = aircv.find_template(screenshot, imgObject, confidence)
     return match
+
+
+def getPlan():
+    excel = xw.Book(app.path + r"\yuhun.xlsx")
+    data = excel.sheets["套装"].range('A1').expand().value
+    planList = []
+    packageList = []
+    # 遍历全部套装
+    num = 1
+    for item in data:
+        if item[0] == "使用场景":
+            continue  # 跳过标题行
+        package = {
+            "编号": num,
+            "使用场景": item[0],
+            "式神": item[1],
+            "御魂1": item[2],
+            "御魂2": item[3],
+            "二号位": keyList[item[4]],
+            "四号位": keyList[item[5]],
+            "六号位": keyList[item[6]],
+            "指标1": indexList[item[7]],
+            "指标2": None if item[8] is None else indexList[item[8]],
+            "备注": item[9],
+        }
+        packageList.append(package)
+        num += 1
+
+        # 两个指标禁止相同或者同时为多属性指标
+        if package["指标1"] == package["指标2"]:
+            package["指标2"] = None
+            logger.info("警告: 存在套装 指标1和指标2相同, 已无视指标2")
+
+        # 指标决定副属性
+        fuKeys = getFuKey(package["指标1"])
+        fuKeys.extend(getFuKey(package["指标2"]))
+        fuKeys = list(set(fuKeys))  # 去重
+        fu = [None, None, None, None]
+        i = 0
+        for f in fuKeys:
+            fu[i] = f
+            i += 1
+
+        planList.append(planData(package["御魂1"], 1, None, fu))
+        planList.append(planData(package["御魂1"], 2, package["二号位"], fu))
+        planList.append(planData(package["御魂1"], 3, None, fu))
+        planList.append(planData(package["御魂1"], 4, package["四号位"], fu))
+        planList.append(planData(package["御魂1"], 5, None, fu))
+        planList.append(planData(package["御魂1"], 6, package["六号位"], fu))
+
+        if package["御魂2"] is not None and package["御魂2"] != package["御魂1"]:
+            planList.append(planData(package["御魂2"], 1, None, fu))
+            planList.append(planData(package["御魂2"], 2, package["二号位"], fu))
+            planList.append(planData(package["御魂2"], 3, None, fu))
+            planList.append(planData(package["御魂2"], 4, package["四号位"], fu))
+            planList.append(planData(package["御魂2"], 5, None, fu))
+            planList.append(planData(package["御魂2"], 6, package["六号位"], fu))
+
+    logger.info(packageList)
+    logger.info(planList)
+
+
+def getFuKey(index):
+    if index is None:
+        return []
+    elif index == "治疗量":
+        return ["生命加成", "暴击", "暴击伤害"]
+    elif index == "伤害输出":
+        return ["攻击加成", "暴击", "暴击伤害"]
+    else:
+        return [index]
+
+
+def planData(yuHun, index, main=None, fu=None):
+    if fu is None:
+        fu = [None, None, None, None]
+
+    if index == 1:
+        m = "攻击"
+    elif index == 3:
+        m = "防御"
+    elif index == 5:
+        m = "生命"
+    else:
+        m = main
+
+    return {
+        "御魂": yuHun,
+        "位置": index,
+        "主属性": m,
+        "副属性1": fu[0],
+        "副属性2": fu[1],
+        "副属性3": fu[2],
+        "副属性4": fu[3],
+    }
+
+
+def ana():
+    getPlan()
+    # readYuHun()
